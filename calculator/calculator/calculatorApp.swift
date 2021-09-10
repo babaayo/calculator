9580134d4236f47dccdbc3eee1a82813ef56ce37
babaayo <41478961+babaayo@users.noreply.github.com>
2021-09-10 12:01:33 -0400

    Add files via upload

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by ola ayo on 2021-09-07.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
